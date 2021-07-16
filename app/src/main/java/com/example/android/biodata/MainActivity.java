bac145a4efa079046c6f1d47fd09ec1226de86ca
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-16 21:21:58 +0530

    FVB ID

--- app/src/main/java/com/example/android/biodata/MainActivity.java
+++ app/src/main/java/com/example/android/biodata/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.android.biodata;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+CheckBox checkBoxstatus;
+RadioButton rbmale , rbfemale ;
+Switch swGrade;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkBoxstatus = findViewById(R.id.checkBoxstatus);
+        rbmale = findViewById(R.id.rbmale);
+        rbfemale = findViewById(R.id.rbfemale);
+        swGrade = findViewById(R.id.switchGrade);
+
+    }
+
+    public void bucheck(View view) {
     }
 }
