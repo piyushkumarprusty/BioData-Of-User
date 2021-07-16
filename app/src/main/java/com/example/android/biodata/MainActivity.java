138dc610fb7fadd68f18e9d3f0563de9bec9009d
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-16 21:55:33 +0530

    dis

--- app/src/main/java/com/example/android/biodata/MainActivity.java
+++ app/src/main/java/com/example/android/biodata/MainActivity.java
@@ -7,11 +7,17 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import java.sql.ResultSet;
 
 public class MainActivity extends AppCompatActivity {
-CheckBox checkBoxstatus;
-RadioButton rbmale , rbfemale ;
-Switch swGrade;
+    CheckBox checkBoxstatus;
+    RadioButton rbmale, rbfemale;
+    Switch swGrade;
+    TextView tvDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +26,30 @@ Switch swGrade;
         rbmale = findViewById(R.id.rbmale);
         rbfemale = findViewById(R.id.rbfemale);
         swGrade = findViewById(R.id.switchGrade);
-
+        tvDisplay = findViewById(R.id.tvResult);
     }
 
     public void bucheck(View view) {
+        String Result = "";
+        if (checkBoxstatus.isChecked()) {
+            Result = "  He is married";
+        } else {
+            Result = "  He is not married";
+        }
+        if ((rbmale.isChecked())) {
+            Result += " Also he is male";
+        } else {
+            Result += " Also she is female";
+        }
+        if (swGrade.isChecked()) {
+            Result += "  Also student is grade";
+        } else {
+            Result += " A lso student is not grade";
+        }
+
+        tvDisplay.setText(Result);
+
+
+
     }
 }
