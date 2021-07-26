1aaa1c414c136dc2b2a12b943229c3963c0258dd
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-26 22:02:45 +0530

    designed

--- app/src/main/java/com/example/android/biodata/MainActivity.java
+++ app/src/main/java/com/example/android/biodata/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.android.biodata;
+ package com.example.android.biodata;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,7 +32,48 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.tvResult);
     }
 
-    public void bucheck(View view) {
+
+
+
+    public void buCheck(View view) {
+
+        // Alert
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setMessage("Are you sure to save")
+                .setTitle("Confirmation")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String Result = "";
+                        if (checkBoxstatus.isChecked()) {
+                            Result = "  He is married";
+                        } else {
+                            Result = "  He is not married";
+                        }
+                        if ((rbmale.isChecked())) {
+                            Result += "  Also he is male";
+                        } else {
+                            Result += "  Also she is female";
+                        }
+                        if (swGrade.isChecked()) {
+                            Result += "  Also student is grade";
+                        } else {
+                            Result += "  Also student is not grade";
+                        }
+
+                        tvDisplay.setText(Result);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Again give input", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+
+    public void buCheck(View view) {
 
         // Alert
 
@@ -73,3 +114,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
+
+
